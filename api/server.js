e1f5e4ee1ed15bf958563eed72d9f6a1c33111bf
ryanp <ryanpanimation@gmail.com>
2020-08-19 21:15:55 -0500

    create authRouter

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const authRouter = require("../auth/authRouter");
+const plantRouter = require("../plants/plantsRouter");
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use("/api/auth", authRouter);
+// server.use("/api/plants", plantRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
