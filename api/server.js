fd11d6de4fec419396affe8da0415ecfc62af0d8
ryanp <ryanpanimation@gmail.com>
2020-08-19 22:30:38 -0500

    authRouter working, jwt and hash pws

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 
 const authRouter = require("../auth/authRouter");
 const plantRouter = require("../plants/plantsRouter");
+const authenticate = require("../auth/authenticateMiddleware");
 
 const server = express();
 
@@ -10,7 +11,7 @@ server.use(express.json());
 server.use(helmet());
 
 server.use("/api/auth", authRouter);
-// server.use("/api/plants", plantRouter);
+// server.use("/api/plants",authenticate, plantRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
