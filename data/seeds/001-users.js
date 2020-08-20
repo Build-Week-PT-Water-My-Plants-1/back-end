b52965422c044d044a83a4cf53c1bbe138201ff7
Erik Scow <erikscow@gmail.com>
2020-08-19 20:20:41 -0700

    fixed issues with seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcryptjs')
+
+const password = 'pass'
+
+const hash = bcrypt.hashSync(password, 12)
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -7,17 +12,17 @@ exports.seed = function(knex) {
       return knex('users').insert([
         {
           username: 'user1',
-          password: 'pass',
+          password: hash,
           phonenumber: '111-111-1111'
         },
         {
           username: 'user2',
-          password: 'pass',
+          password: hash,
           phonenumber: '222-222-2222'
         },
         {
           username: 'user3',
-          password: 'pass',
+          password: hash,
           phonenumber: '333-333-3333'
         }
       ]);
