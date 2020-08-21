4e5e9fe850b6f0f79288578d246313022cf7f604
ryanp <ryanpanimation@gmail.com>
2020-08-20 21:55:41 -0500

    create cleaner seed data

--- data/seeds/002-plants.js
+++ data/seeds/002-plants.js
@@ -1,40 +1,34 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('plants').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('plants').insert([
-        {
-          user_id: 1,
-          nickname: 'the nickname',
-          species: 'species 1',
-          h2ofrequency: 'once daily'
-        },
-        {
-          user_id: 1,
-          nickname: 'the nickname',
-          species: 'species 2',
-          h2ofrequency: 'twice daily'
-        },
-        {
-          user_id: 2,
-          nickname: 'the nickname',
-          species: 'species 1',
-          h2ofrequency: 'once daily'
-        },
-        {
-          user_id: 2,
-          nickname: 'the nickname',
-          species: 'species 2',
-          h2ofrequency: 'twice daily'
-        },
-        {
-          user_id: 2,
-          nickname: 'the nickname',
-          species: 'species 3',
-          h2ofrequency: 'every other day'
-        }
-      ]);
-    });
+exports.seed = function (knex) {
+  return knex("plants").insert([
+    {
+      user_id: 1,
+      nickname: "the nickname",
+      species: "species 1",
+      h2ofrequency: "once daily",
+    },
+    {
+      user_id: 1,
+      nickname: "the nickname",
+      species: "species 2",
+      h2ofrequency: "twice daily",
+    },
+    {
+      user_id: 2,
+      nickname: "the nickname",
+      species: "species 1",
+      h2ofrequency: "once daily",
+    },
+    {
+      user_id: 2,
+      nickname: "the nickname",
+      species: "species 2",
+      h2ofrequency: "twice daily",
+    },
+    {
+      user_id: 2,
+      nickname: "the nickname",
+      species: "species 3",
+      h2ofrequency: "every other day",
+    },
+  ]);
 };
