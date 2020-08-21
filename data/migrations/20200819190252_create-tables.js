a31e420b7160259bf9b2d767332e70ee9a2ac5c4
ryanp <ryanpanimation@gmail.com>
2020-08-20 22:23:41 -0500

    create table schema, update readme

--- data/migrations/20200819190252_create-tables.js
+++ data/migrations/20200819190252_create-tables.js
@@ -1,36 +1,20 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
-    .createTable('users', tbl => {
-        tbl.increments()
-        tbl.text('username')
-            .unique()
-            .notNullable()
-        tbl.text('password')
-            .notNullable()
-        tbl.text('phonenumber')
-            .unique()
-            .notNullable()
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.text("username").unique().notNullable();
+      tbl.text("password").notNullable();
+      tbl.text("phonenumber").notNullable();
     })
-    .createTable('plants', tbl => {
-        tbl.increments()
-        tbl.integer('user_id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-        tbl.text('nickname')
-            .notNullable()
-        tbl.text('species')
-            .notNullable()
-        tbl.text('h2ofrequency')
-            .notNullable()
-    })
-
+    .createTable("plants", (tbl) => {
+      tbl.increments();
+      tbl.integer("user_id").unsigned().notNullable().references("id").inTable("users");
+      tbl.text("nickname").notNullable();
+      tbl.text("species").notNullable();
+      tbl.text("h2ofrequency").notNullable();
+    });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('plants')
-    .dropTableIfExists('users')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("plants").dropTableIfExists("users");
 };
