a906e0898314439e7d3bf492811da125fa5aa435
ryanp <ryanpanimation@gmail.com>
2020-08-22 00:10:18 -0500

    create server test file

---
+++ api/server.test.js
@@ -0,0 +1,7 @@
+const { expectCt } = require("helmet");
+
+describe("server.js", () => {
+  test("should be in testing mode", async () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+});
