ad46124deb28f35f6a3b6e8df9bac39a0fb6258f
ryanp <ryanpanimation@gmail.com>
2020-08-22 00:06:07 -0500

    changes to models for postgres

--- auth/authModel.js
+++ auth/authModel.js
@@ -1,24 +1,43 @@
-const db = require('../data/dbConfig')
+const db = require("../data/dbConfig");
 
 module.exports = {
-    findById,
-    register,
-    findByUsername,
-}
+  findById,
+  register,
+  findByUsername,
+  update,
+  remove,
+};
 
 function findById(id) {
-    return db('users')
-    .where({ id }).first()
-    .select("username", "phonenumber").first()
+  return db("users").where({ id }).first().select("username", "phonenumber");
 }
 
-function register(userInfo) {
-    return db('users').insert(userInfo)
-        .then(ids => {
-            return findById(ids[0])
-        })
-}
+// function register(userInfo) {
+//   return db("users")
+//     .insert(userInfo)
+//     .then((ids) => {
+//       return findById(ids[0]);
+//     });
+// }
+
+const register = async (userInfo) => {
+  const userId = await db("users").insert(userInfo).returning("id");
+  const data = await findById(userId[0]);
+  return data;
+};
 
 function findByUsername(username) {
-    return db('users').where({ username }).first()
+  return db("users").where({ username }).first();
 }
+
+const update = async (user) => {
+  const userId = await db("users").where({ id: user.id }).insert(user).returning("id");
+  const data = await findById(userId[0]);
+  return data;
+};
+
+const remove = async (id) => {
+  const userInfo = await findById(id);
+  await db("users").where({ id }).del();
+  return userInfo;
+};
--- plants/plantsModel.js
+++ plants/plantsModel.js
@@ -1,40 +1,60 @@
-const db = require('../data/dbConfig')
+const db = require("../data/dbConfig");
 
 module.exports = {
-    findUserById,
-    findPlantsByUserId,
-    findById,
-    add,
-    update,
-    remove
-}
+  findUserById,
+  findPlantById,
+  findPlantsByUserId,
+  add,
+  update,
+  remove,
+};
+
+//? could also write it this way
+// const findBy = (id, db) => {
+//   db(db).where({ id });
+// };
 
 function findUserById(id) {
-    return db('users').where({ id })
+  return db("users").where({ id }).first();
 }
 
-function findById(id){
-    return db('plants').where({ id })
+function findPlantById(id) {
+  return db("plants").where({ id }).first();
 }
 
-function findPlantsByUserId(userId) {
-    return db('plants').where({ user_id: userId})
+function findPlantsByUserId(user_id) {
+  return db("plants").where({ user_id });
 }
 
-function add(plantInfo) {
-    return db('plants').insert(plantInfo)
-        .then( ids => {
-            return findById(ids[0])
-        })
-}
+// function add(plantInfo) {
+//   return db("plants")
+//     .insert(plantInfo)
+//     .then((ids) => {
+//       return findById(ids[0]);
+//     });
+// }
 
-function update(newInfo, id) {
-    return db('plants').where({ id }).update(newInfo)
-        .then( ids => {
-            return findById(id)
-        })
-}
+const add = async (plantInfo) => {
+  const plantId = await db("plants").insert(plantInfo).returning("id");
+  const data = await findPlantById(plantId[0]).first();
+  return data;
+};
 
-function remove(id){
-    return db('plants').where({ id }).del()
+// function update(newInfo, id) {
+//   return db("plants")
+//     .where({ id })
+//     .update(newInfo)
+//     .then((ids) => {
+//       return findById(id);
+//     });
+// }
+
+const update = async (newInfo, id) => {
+  const plantId = await db("plants").where({ id }).update(newInfo).returning("id");
+  return await findPlantById(plantId[0]);
+};
+
+async function remove(id) {
+  const plantData = await plants.findById(id);
+  return db("plants").where({ id: plantData[0] }).del();
 }
